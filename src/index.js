188067dcf01c90677004304af32ec3974d178f08
otvv <suizld@pm.me>
2020-10-24 18:18:03 -0300

    :package: v0.0.6
    ADDED:
    added a new check to see if the post id is valid or not
    
    CHANGED:
    bumped package version
    refactored the code
    fixed typos on comments
    removed useless code (unused try block)

--- src/index.js
+++ src/index.js
@@ -4,72 +4,72 @@
 // license: MIT
 
 // modules
-const { Builder, By, Key, until } = require('selenium-webdriver')
-const Firefox = require('selenium-webdriver/firefox')
-const Random = require('random')
-const colorPrint = require('colorprint')
+const { Builder, By, Key, until } = require("selenium-webdriver")
+const Firefox = require("selenium-webdriver/firefox")
+const Random = require("random")
+const colorPrint = require("colorprint")
 
 // external configs
-const accountsJSON = require('./config/accounts.json');
-const messagesJSON = require('./config/messages.json');
+const accountsJSON = require("./config/accounts.json");
+const messagesJSON = require("./config/messages.json");
 
 (async function main() {
 
   // handle environment variables
-  const repArguments = process.argv.splice(2)
+  const repArguments = process.argv.splice(2);
 
   // post id (first variable)
-  const postID = repArguments[0]
+  const postID = repArguments[0];
 
   if (!postID) {
 
-    colorPrint.warn('[UC-REP] - you need to add a post id first')
+    colorPrint.warn("[UC-REP] - you need to add a post id first")
     return
   }
 
   // reputation type
-  const repType = repArguments[1]
+  const repType = repArguments[1];
 
   if (!repType) {
 
-    colorPrint.warn('[UC-REP] - please type in the reputation type (positive/negative)')
+    colorPrint.warn("[UC-REP] - please type in the reputation type (positive/negative)")
     return
   }
 
-  // this will keep the code running until we ran out of accounts
-  while (true) {
+  // this will keep the code running until we run out of accounts
+  for (;;) {
 
     const webDriver = new Builder()
-      .forBrowser('firefox')
-      .setFirefoxOptions(new Firefox.Options().setPreference('browser.privatebrowsing.autostart', true).headless())
-      .build()
+      .forBrowser("firefox")
+      .setFirefoxOptions(new Firefox.Options().setPreference("browser.privatebrowsing.autostart", true).headless())
+      .build();
 
-    // give custom reputation reason (second variable)
-    const giveReason = repArguments[2] == true ? true : false
+    // give custom reputation reason
+    const giveReason = repArguments[2] === true ? true : false;
 
-    colorPrint.trace('[UC-REP] - opening main page')
+    colorPrint.trace("[UC-REP] - opening main page");
 
     // open main unknowncheats page
-    await webDriver.get('https://www.unknowncheats.me/forum/index.php')
+    await webDriver.get("https://www.unknowncheats.me/forum/index.php");
 
-    // check if the main poge is loaded
-    const isMainPageLoaded = await webDriver.wait(until.urlIs('https://www.unknowncheats.me/forum/index.php'))
+    // check if the main page is loaded
+    const isMainPageLoaded = await webDriver.wait(until.urlIs("https://www.unknowncheats.me/forum/index.php"));
 
     if (isMainPageLoaded) {
 
-      colorPrint.trace('[UC-REP] - main page opened')
+      colorPrint.trace("[UC-REP] - main page opened");
 
       // store account username & password here
-      let { username, password } = ''
+      let { username, password } = "";
       
       username = accountsJSON.Username[Random.int(0, accountsJSON.Username.length)] // FIXME: potential crash
       password = accountsJSON.Password[Random.int(0, accountsJSON.Password.length)] // FIXME: potential crash
 
       // TODO: this check is kinda useless right now. The goal here is to make some sort of container to store the accounts and then 
-      // somehow, delete them from the container once they're used. After that check if the container is empty and stop the bot from running
+      // somehow, delete them from the container once they"re used. After that, check if the container is empty and stop the bot
       if (!username || !password) {
 
-        colorPrint.error('[UC-REP] - ran out of accounts')
+        colorPrint.error("[UC-REP] - the bot has ran out of accounts");
 
         // exit
         webDriver.quit();
@@ -77,61 +77,61 @@ const messagesJSON = require('./config/messages.json');
       }
 
       // username
-      await webDriver.findElement(By.id('navbar_username')).sendKeys(username)
+      await webDriver.findElement(By.id("navbar_username")).sendKeys(username);
 
       // password
-      await webDriver.findElement(By.id('Password1')).sendKeys(password, Key.RETURN)
+      await webDriver.findElement(By.id("Password1")).sendKeys(password, Key.RETURN);
 
-      colorPrint.trace('[UC-REP] - attempting to log in')
+      colorPrint.trace("[UC-REP] - attempting to log in");
       
       // check if the account has sucessfully logged in
-      const loggedIn = await webDriver.wait(until.urlIs('https://www.unknowncheats.me/forum/login.php'))
+      const loggedIn = await webDriver.wait(until.urlIs("https://www.unknowncheats.me/forum/login.php"));
 
       if (loggedIn) {
 
-        colorPrint.trace('[UC-REP] - account logged in')
+        colorPrint.trace("[UC-REP] - account logged in");
 
         // open post to give reputation
-        await webDriver.get('https://www.unknowncheats.me/forum/' + postID + '-post.html')
+        await webDriver.get("https://www.unknowncheats.me/forum/" + postID + "-post.html");
 
         // wait for it to load all elements
-        await webDriver.wait(until.urlIs('https://www.unknowncheats.me/forum/' + postID + '-post.html'))
-
-        try {
+        const foundPost = await webDriver.wait(until.urlIs("https://www.unknowncheats.me/forum/" + postID + "-post.html"));
+        
+        // if the post id is invalid
+        if (!foundPost) {
 
           // check if the post id is valid
-          await webDriver.findElement(By.xpath('//*[contains(text(), "Invalid Post specified")]'))
+          await webDriver.findElement(By.xpath("//*[contains(text(), 'Invalid Post specified')]"));
 
-          colorPrint.fatal('[UC-REP] - unknown post id')
+          colorPrint.fatal("[UC-REP] - unknown post id");
 
           // exit
           webDriver.quit();
           break;
+        }
 
-        } catch (e) { }
+        colorPrint.trace("[UC-REP] - found post id: " + postID);
 
-        colorPrint.trace('[UC-REP] - found post id: ' + postID)
+        // store random reputation messages here
+        let message = "";
 
-        // store random reputation messages
-        let message = ''
-
-        // check which type of rep we're going to add
-        if (repType === 'positive') {
+        // check which type of rep we"re going to add
+        if (repType === "positive") {
 
           message = messagesJSON.Positive[Random.int(0, messagesJSON.Positive.length)] // FIXME: potential crash
 
-          colorPrint.info('[UC-REP] - giving positive rep')
+          colorPrint.info("[UC-REP] - giving positive rep");
 
           // open reputation box
-          await webDriver.findElement(By.id('reputation_' + postID + '-pos')).click()
-        } else if (repType === 'negative') {
+          await webDriver.findElement(By.id("reputation_" + postID + "-pos")).click();
+        } else if (repType === "negative") {
 
           message = messagesJSON.Negative[Random.int(0, messagesJSON.Negative.length)] // FIXME: potential crash
 
-          colorPrint.error('[UC-REP] - giving negative rep')
+          colorPrint.error("[UC-REP] - giving negative rep");
 
           // open reputation box
-          await webDriver.findElement(By.id('reputation_' + postID + '-neg')).click()
+          await webDriver.findElement(By.id("reputation_" + postID + "-neg")).click();
         }
 
         setTimeout(async () => {
@@ -139,25 +139,25 @@ const messagesJSON = require('./config/messages.json');
           if (giveReason) {
 
             // write a random reputation reason 
-            await webDriver.findElement(By.id('reason_' + postID)).sendKeys(message)
+            await webDriver.findElement(By.id("reason_" + postID)).sendKeys(message);
 
-            colorPrint.trace('[UC-REP] - giving reputation reason: ' + message)
+            colorPrint.trace("[UC-REP] - giving reputation reason: " + message);
           } else {
 
-            colorPrint.warn('[UC-REP] - no reputation reason specified')
+            colorPrint.warn("[UC-REP] - no reputation reason specified");
           }
 
           // check if the reputation box is opened
-          const isRepBoxOpened = await webDriver.findElement(By.xpath('//*[contains(text(), "Reputation")]'))
+          const isRepBoxOpened = await webDriver.findElement(By.xpath("//*[contains(text(), 'Reputation')]"));
 
           // only continue if the reputation box is opened
           if (isRepBoxOpened) {
 
             // give the post reputation
-            await webDriver.findElement(By.id('reputationsubmit_' + postID)).click()
+            await webDriver.findElement(By.id("reputationsubmit_" + postID)).click();
           }
 
-          colorPrint.notice('[UC-REP] - finished')
+          colorPrint.notice("[UC-REP] - finished");
 
           // exit
           webDriver.quit();
